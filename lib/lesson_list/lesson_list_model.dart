75814daa416bec0bcf16c1a747657495bb9e2b34
Takashi Fukuyama <osakalivetheater@gmail.com>
2022-05-30 23:15:41 +0900

    FireStoreのgetメソッドで値を取得

--- lib/lesson_list/lesson_list_model.dart
+++ lib/lesson_list/lesson_list_model.dart
@@ -3,23 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:osakalivetheater3/domain/lesson.dart';
 
 class LessonListModel extends ChangeNotifier {
-  final Stream<QuerySnapshot> _usersStream =
-  FirebaseFirestore.instance.collection('lessons').snapshots();
-
   List<Lesson>? lessons;
 
-  void fetchLessonList() {
-    _usersStream.listen((QuerySnapshot snapshot) {
-      final List<Lesson> lessons = snapshot.docs.map((
-          DocumentSnapshot document) {
-        Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-        final String tech = data['tech'];
-        final String stage = data['stage'];
-        return Lesson(stage, tech);
-      }).toList();
+  void fetchLessonList() async{
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('lessons').get();
+
+    final List<Lesson> lessons = snapshot.docs.map((
+        DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String tech = data['tech'];
+      final String stage = data['stage'];
+      return Lesson(stage, tech);
+    }).toList();
 
       this.lessons = lessons;
       notifyListeners();
-    });
   }
 }
