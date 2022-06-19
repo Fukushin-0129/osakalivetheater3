68cfbcb77e8614c4ae34f59169fd5ea6a4166fd0
Takashi Fukuyama <osakalivetheater@gmail.com>
2022-06-19 20:24:07 +0900

    新バージョンslideableあとエラ１個

--- lib/lesson_list/lesson_list_page.dart
+++ lib/lesson_list/lesson_list_page.dart
@@ -5,7 +5,6 @@ import 'package:osakalivetheater3/lesson_list/lesson_list_model.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:osakalivetheater3/domain/lesson.dart';
-
 import '../edit_lesson/edit_lesson_page.dart';
 
 class LessonListPage extends StatelessWidget {
@@ -13,103 +12,104 @@ class LessonListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<LessonListModel>(
-        create: (_) => LessonListModel()..fetchLessonList(),
+      create: (_) => LessonListModel()..fetchLessonList(),
       child: Scaffold(
         appBar: AppBar(
           title: Text('テクニック一覧'),
         ),
         body: Center(
           child:Consumer<LessonListModel>(builder: (context, model, child) {
-            final List<Lesson>? lessons = model.lessons;
+    final List<Lesson>? lessons = model.lessons;
 
-            if(lessons == null){
-              return CircularProgressIndicator();
-            }
-            final List<Widget> widgets = lessons
-                .map(
-                  (lessons) => Slidable(
-                actionPane: SlidableDrawerActionPane(),
-                child: ListTile(
-                    title: Text(lessons.tech),
-                    subtitle: Text(lessons.stage),
-                ),
-                  secondaryActions: <Widget>[
-                  IconSlideAction(
-                    caption: '編集',
-                    color: Colors.black45,
-                    icon: Icons.edit,
-                    onTap: () async {
-                      // 編集画面に遷移
+    if(lessons == null){
+    return CircularProgressIndicator();
+    }
+    final List<Widget> widgets = lessons
+        .map(
+    (lessons) =>
 
-                      // 画面遷移
-                      final String? title = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => EditLessonPage(lessons),
-                        ),
-                      );
+    Slidable(
+    endActionPane: ActionPane(
+    motion: DrawerMotion(),
+    children: [
+    SlidableAction(
+    onPressed: (value) async {
+    //画面遷移
+    final String? title = await Navigator.push(
+    context,
+    MaterialPageRoute(
+    builder: (context) => EditLessonPage(lessons),
+    ),
+    );
 
-                      if (title != null) {
-                        final snackBar = SnackBar(
-                          backgroundColor: Colors.green,
-                          content: Text('$titleを編集しました'),
-                        );
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(snackBar);
-                      }
+    if (title != null) {
+    final snackBar = SnackBar(
+    backgroundColor: Colors.green,
+    content: Text('$titleを編集しました'),
+    );
+    ScaffoldMessenger.of(context)
+        .showSnackBar(snackBar);
+    }
 
-                      model.fetchLessonList();
-                    },
-                  ),
-                    IconSlideAction(
-                    caption: '削除',
-                    color: Colors.red,
-                    icon: Icons.delete,
-                    onTap: () async {
-                      // 削除しますか？って聞いて、はいだったら削除
-                      await showConfirmDialog(context, lessons, model);
-                    },
-                  ),
-                ],
-              ),
-            )
-                .toList();
-            return ListView(
-              children: widgets,
-            );
-          }),
+    model.fetchLessonList();
+    },
+    backgroundColor: Colors.black45,
+    icon: Icons.edit,
+    label: '編集',
+    ),
+
+
+    SlidableAction(onPressed: (value) {
+    // 削除しますか？って聞いて、はいだったら削除
+    },
+    backgroundColor: Colors.red,
+    icon: Icons.delete,
+    label: '削除',
+    ),
+    ],
+    ),
+
+    child: ListTile(
+    title: Text(lessons.tech),
+    subtitle: Text(lessons.stage),
+    ) ,
+    ),
+    )
+        .toList();
+                    return ListView(
+                    children: widgets,
+    );
+    }),
         ),
-        floatingActionButton:
-        Consumer<LessonListModel>(builder: (context, model, child) {
-          return FloatingActionButton(
-            onPressed: () async {
-              // 画面遷移
-              final bool? added = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => AddLessonPage(),
-                  fullscreenDialog: true,
-                ),
-              );
 
-              if (added != null && added) {
-                final snackBar = SnackBar(
-                  backgroundColor: Colors.green,
-                  content: Text('テクニックを追加しました'),
-                );
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              }
-
-              model.fetchLessonList();
-            },
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
+              floatingActionButton:
+              Consumer<LessonListModel>(builder: (context, model, child) {
+                 return FloatingActionButton(
+    onPressed: () async {
+    // 画面遷移
+    final bool? added = await Navigator.push(
+    context,
+    MaterialPageRoute(
+                    builder: (context) => AddLessonPage(),
+                    fullscreenDialog: true,
+    ),
+    );
+    if (added != null && added) {
+    final snackBar = SnackBar(
+    backgroundColor: Colors.green,
+    content: Text('テクニックを追加しました'),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+    model.fetchLessonList();
+    },
+    tooltip: 'Increment',
+    child: Icon(Icons.add),
           );
         }),
-      ),
-    );
+       );
   }
-
+  }
   Future showConfirmDialog(
       BuildContext context,
       Lesson lesson,
@@ -146,4 +146,3 @@ class LessonListPage extends StatelessWidget {
       },
     );
   }
-}
